3a2863c97a16bb26844950e42abadd06cea5333a
Vivien <viviensiu@gmail.com>
2024-12-08 15:12:42 +0000

    update with module_9_homework

---
+++ 9_serverless/lambda_function_hw9.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import tflite_runtime.interpreter as tflite
+from keras_image_helper import create_preprocessor
+
+
+preprocessor = create_preprocessor('xception', target_size=(200, 200))
+
+classes = ['prediction']
+
+interpreter = tflite.Interpreter(model_path='model_2024_hairstyle_v2.tflite')
+interpreter.allocate_tensors()
+
+input_index = interpreter.get_input_details()[0]['index']
+output_index = interpreter.get_output_details()[0]['index']
+
+def predict(url):
+    X = preprocessor.from_url(url)
+
+    interpreter.set_tensor(input_index, X)
+    interpreter.invoke()
+    preds = interpreter.get_tensor(output_index)
+
+    float_predictions = preds[0].tolist()
+
+    return dict(zip(classes, float_predictions))
+
+
+def lambda_handler(event, context):
+    url = event['url']
+    result = predict(url)
+    return result
+
+
